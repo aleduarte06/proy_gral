b4bd53bd66e334876a3f2ffb49ada19d3616b490
aleduarte06 <aleduarte06@gmail.com>
2015-09-05 18:12:25 -0300

    obteniendo y mostrando los resultados de meli listo

--- js/app.js
+++ js/app.js
@@ -4,18 +4,18 @@
 var mod = angular.module('myapp',['ui.router']); //Creamos un modulo de angular
 mod.config(function($stateProvider, $urlRouterProvider){
 
- $stateProvider
-     .state('home',{
+    $stateProvider
+        .state('home',{
          url: '/home',
          templateUrl: 'templates/buscador.html',
          controller: 'buscadorCtrl'
-     })
+        })
 
-     .state('resultados',{
+        .state('resultados',{
          url: '/resultados?q',
          templateUrl: 'templates/resultados.html',
          controller: 'resultadosCtrl'
-     })
+        });
     $urlRouterProvider.otherwise('/home');
 
 });
@@ -32,7 +32,7 @@ mod.controller('buscadorCtrl', function($scope,$state,$http){
 mod.controller('resultadosCtrl', function($scope,$http,$stateParams){
     console.log('Segungo controlador');
     //console.log($stateParams.q);
-    var url = 'https://api.mercadolibre.com/sites/MLA/search?q='+ $stateParams.q + '&limit=10';
+    var url = 'http://localhost:8045/meliproxy?q='+ $stateParams.q + '&limit=10';
     console.log(url);
     $http.get(url).then(function(res){
         console.log(res);
